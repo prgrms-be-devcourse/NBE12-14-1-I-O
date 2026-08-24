59e6867bb22a69d2ca669c301a8eb02c5535fc97
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-08-24 17:56:08 +0900

    init

---
+++ project/src/test/java/io/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
