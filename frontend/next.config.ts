4f1ff71f0c50a0ee68a10646404dd8bb6327b89a
swshindev-beep <swshin.dev@gmail.com>
2026-08-27 12:59:50 +0900

    chore: next.config.ts 외부 이미지 허용 보안 설정 추가

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // 로컬 서버(localhost:8080)이미지 허용 보안 설정
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
