f8b3ee307d38728666967d1d5972a10bdcf71a66
kelly11998-gif <kelly11998@gmail.com>
2026-08-27 09:36:09 +0900

    [feat] 주문목록 API구현 (#9)
    * feat: 주문목록 API구현
    
    * chore: 잘못 커밋된 테스트 클래스 삭제

---
+++ project/src/main/java/io/project/domain/order/dto/OrderListResponse.java
@@ -0,0 +1,32 @@
+package io.project.domain.order.dto;
+
+import io.project.domain.delivery.entity.DeliveryStatus;
+import io.project.domain.order.entity.Order;
+import io.project.domain.order.entity.OrderStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record OrderListResponse(
+        int orderId,
+        LocalDateTime orderedAt,
+        OrderStatus orderStatus,
+        DeliveryStatus deliveryStatus,
+        String address,
+        String postalCode,
+        List<OrderItemResponse> orderItemResponses
+){
+    public OrderListResponse(Order order){
+        this(
+                order.getId(),
+                order.getOrderedAt(),
+                order.getStatus(),
+                order.getDelivery().getStatus(),
+                order.getDelivery().getAddress(),
+                order.getDelivery().getPostalCode(),
+                order.getOrderItems().stream()
+                        .map(OrderItemResponse::new)
+                        .toList()
+        );
+    }
+}
