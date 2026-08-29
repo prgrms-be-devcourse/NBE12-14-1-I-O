223df97162ce9bad070774a6602495e4b17ffe08
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-08-29 15:43:21 +0900

    feat: 대시보드 테스트 추가 - 미완성

--- project/src/main/java/io/project/global/initData/ProductInitData.java
+++ project/src/main/java/io/project/global/initData/ProductInitData.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 @RequiredArgsConstructor
 public class ProductInitData {
 
